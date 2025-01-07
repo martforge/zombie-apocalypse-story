6a3f60b72d88efdee6bc51305cd28279d2257ff4
Marion Joseph Agustin <131208352+martforge@users.noreply.github.com>
2025-01-07 16:56:03 +0800

    add files

---
+++ zombie-apocalypse-story.py
@@ -0,0 +1,53 @@
+# Zombie Apocalypse
+import random
+
+class Survivor:
+    def __init__(self, name, occupation, skill):
+        self.name = name
+        self.occupation = occupation
+        self.skill = skill
+
+    def __str__(self):
+        return f'{self.name} - {self.occupation} with skill: {self.skill}'
+
+names = [
+    "Jin",    # Chinese/Korean (Asia)
+    "Aya",    # Japanese (Asia)
+    "Ravi",   # Indian (Asia)
+    "Zara",   # Arabic (Asia)
+    "Liam",   # Irish (Europe)
+    "Olivia", # U.S. (North America)
+    "Jackson",# U.S. (North America)
+    "Emma",   # U.S. (North America)
+    "Sofia",  # Spanish (Europe)
+    "Kai"     # Hawaiian (North America)
+]
+
+occupations_skill = {
+    "Doctor": "First Aid",
+    "Engineer": "Building",
+    "Farmer": "Food Production",
+    "Chef": "Cooking",
+    "Mechanic": "Vehicle Repair",
+    "Soldier": "Combat",
+    "Teacher": "Leadership",
+    "Hunter": "Tracking",
+    "Scavenger": "Resource Gathering",
+    "Scientist": "Research"
+}
+
+# character creation
+def creation():
+    name = random.choice(names)
+    occupation = random.choice(list(occupations_skill.keys()))
+    skill = occupations_skill[occupation]
+    survivor = Survivor(name, occupation, skill)
+    return survivor
+
+def main():
+    survivor = creation()
+    print(survivor)
+
+
+if __name__ == '__main__':
+    main()
